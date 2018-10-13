50342f21bfce8a3446f10fd0a8268843b72a8466
ctinney <ctinney@rossvideo.com>
2018-10-13 14:24:09 -0400

    Capitalize title correctly

--- application/views/static/header.php
+++ application/views/static/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Global Herizons</title>
+    <title>Global HERizons</title>
     <link rel="stylesheet" href="/assets/css/bootstrap.css?<?=V?>">
     <link rel="stylesheet" href="/assets/css/all.css?<?=V?>">
 </head>
