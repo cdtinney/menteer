ec4586fc96d6a715fc6910e489d2eee99d438a8e
ctinney <ctinney@rossvideo.com>
2018-10-13 14:12:01 -0400

    Update title for all pages

--- application/views/static/header.php
+++ application/views/static/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menteer</title>
+    <title>Global Herizons</title>
     <link rel="stylesheet" href="/assets/css/bootstrap.css?<?=V?>">
     <link rel="stylesheet" href="/assets/css/all.css?<?=V?>">
 </head>
